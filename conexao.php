78422921ce2647f81dba4b6d64579cfc2bcdf3c3
memisju <125404782+lopeslima@users.noreply.github.com>
2023-04-03 16:05:32 -0300

    tenpo

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+//variaveis da conexao do banco de dados
+    $servidor="localhost";
+    $usuario="id20540587_pedro";
+    $senha="Computador10@";
+    $nomedb="id20540587_lapiz";// nome do banco de dados que sera usado
+    //cria a conexao com o banco de dados
+    $conn= new mysqli ($servidor, $usuario, $senha, $nomedb);
+    // checa a conexao
+    if ($conn->connect_error) {
+        die ("conexao falhou:".$conn->connect_error);
+       ?>
+        
